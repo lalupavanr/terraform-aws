35de04389b46b297a14ccc0c9afb8d7e52669435
lalupavanr <pavanrlalu@gmail.com>
2023-04-20 18:44:33 +0530

    add nacl rules

--- firewalls.tf
+++ firewalls.tf
@@ -6,3 +6,29 @@ resource "aws_network_acl" "lms-nacls" {
   }
 
 }
+
+# Add Inbound Rules 
+resource "aws_network_acl_rule" "lms-inbound-nacl-rule" {
+  network_acl_id = aws_network_acl.lms-nacls.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 65535
+
+}
+
+# Add Outbound Rules
+resource "aws_network_acl_rule" "lms-outbound-nacl-rule" {
+  network_acl_id = aws_network_acl.lms-nacls.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 65535
+
+}
