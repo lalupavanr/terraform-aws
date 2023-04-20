189fa250fd620187259857f9b28c0951e0a57bb3
lalupavanr <pavanrlalu@gmail.com>
2023-04-20 16:14:42 +0530

    change

--- vpc.tf
+++ vpc.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "lms1" {
 # Subnet
 resource "aws_subnet" "lms-subnet1" {
   vpc_id                  = aws_vpc.lms1.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = "true"
 
   tags = {
