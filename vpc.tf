9ed146646ba2b439b7ac2bde9a04313993d5cc73
lalupavanr <pavanrlalu@gmail.com>
2023-04-20 16:04:54 +0530

    create subnet

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,14 @@ resource "aws_vpc" "lms1" {
     Name = "lms1"
   }
 }
+
+# Subnet
+resource "aws_subnet" "lms-Subnet1" {
+  vpc_id     = aws_vpc.lms1.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "lms-Subnet1"
+  }
+}
